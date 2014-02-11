33db16e0a45d0ef4f02c822d2ed38dab2799bc00
Hektor Wallin <hektorw@gmail.com>
2014-02-11 22:24:45 +0100

    added off function

--- dev/scripts/events.js
+++ dev/scripts/events.js
@@ -5,6 +5,8 @@
  * @mail hektorw@gmail.com
  */
 
+/* global define */
+
 (function (factory) {
   if(typeof define !== 'undefined' && define.amd) {
     define([], factory);
@@ -14,26 +16,54 @@
     window.Events = factory();
   }
 }(function() {
-
-  // listening to all
-  // 
-  // function: off
-  // function: once
-
+  'use strict';
 
   var slice = Array.prototype.slice;
 
   var Events = {
-    on: function(event, callback, context) {
+
+    // listen to an event
+    on: function(event, callback, context /* opt */) {
+      if(!event || typeof callback !== 'function')
+        throw "event type and callback of type function is required arguments";
+
       this._events || (this._events = {});
       (this._events[event] || (this._events[event] = [])).unshift({ // insert in reverse order
         callback: callback,
+        _ctx: context, // private ctx, used in off method for comparison
         ctx: context || this
       });
       return this;
     },
 
-    off: function() { return this; },
+    off: function(event, callback, ctx) {
+      var key, i, ev, data, _cb, _ctx, events;
+
+      if(!event && !callback && !ctx) {
+        this._events = {};
+        return this;
+      }
+
+      // if no event supplied we iterate over all
+      events = event ? [this._events[event]] : this._events;
+
+      for(key in events) {
+        if(!events.hasOwnProperty(key))
+          continue;
+
+        ev = events[key];
+        for(i = ev.length; i--; ) {
+          data = ev[i];
+          _cb = data.callback === callback;
+          _ctx = data._ctx && data._ctx === ctx;
+          if ((_cb && _ctx) || (!callback && _ctx) || (!ctx && _cb)) {
+            ev.splice(i,1);
+          }
+        }
+      }
+
+      return this;
+    },
 
     trigger: function(event) {
       if(!this._events) return this;
@@ -45,9 +75,9 @@
   };
 
   function _triggerEvents(events, args) {
-    var ev, i = events.length;
-    for( ; i--; ) {
-      (ev = events[i]).callback.apply(ev.ctx, arguments);
+    var ev, i;
+    for(i = events.length ; i--; ) {
+      (ev = events[i]).callback.apply(ev.ctx, args);
     }
   }
 
