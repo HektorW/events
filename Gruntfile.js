d1c3f5ed6f6528dc12d3c526ba064c783959e868
Hektor Wallin <hektorw@gmail.com>
2014-12-07 16:14:29 +0100

    updated file struct and setup build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,129 +1,74 @@
-"use strict";
+/* global module */
 
 module.exports = function(grunt) {
+  "use strict";
+
   require('time-grunt')(grunt);
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
     app: {
-      dev: 'dev',
-      dist: 'dist'
-    },
-
-    watch: {
-      options: {
-        nospawn: false
-      },
-      styles: {
-        files: ['<%= app.dev %>/styles/*.less'],
-        tasks: ['less:dev', 'autoprefixer:dev']
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        },
-        files: [
-          '<%= app.dev %>/*.html',
-          '<%= app.dev %>/scripts/*.js',
-          '<%= app.dev %>/styles/*.css'
-        ]
-      }
-    },
-
-    connect: {
-      options: {
-        port: 9000,
-        livereload: 35729,
-        // hostname: '192.168.0.192'
-        hostname: null
-      },
-      livereload: {
-        options: {
-          // open: true,
-          base: '<%= app.dev %>'
-        }
-      }/*,
-      test: {
-        options: {
-          base: [
-            'test',
-            '<%= app.dev %>'
-          ]
-        }
-      }*/
-    },
-
-    // mocha: {
-    //   all: {
-    //     options: {
-    //       run: true,
-    //       urls: [ 'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html' ]
-    //     }
-    //   }
-    // },
-
-    less: {
-      options: {
-        files: [
-        ]
-      },
-      dev: {
-        files: {
-          '<%= app.dev %>/styles/main.css': '<%= app.dev %>/styles/*.less',
-        }
-      },
-      dist: {
-        files: {
-          '<%= app.dist %>/styles/main.css': '<%= app.dev %>/styles/*.less',
-        }
-      }
-    },
-
-    autoprefixer: {
-      options: {
-      },
-      dev: {
-        src: '<%= app.dev %>/styles/main.css'
-      },
-      dist: {
-        src: '<%= app.dist %>/styles/main.css'
-      }
+      dev: 'src',
+      dist: 'dist',
+      tmp: 'tmp'
     },
 
     clean: {
-      dev: {
-      },
-      dist: {
+      prebuild: {
         files: [{
           dot: true,
           src: [
+            '<%= app.tmp %>',
             '<%= app.dist %>'
           ]
         }]
+      },
+      postbuild: {
+        files: [{
+          dot: true,
+          src: [
+            '<%= app.tmp %>'
+          ]
+        }]
+      }
+    },
+
+    copy: {
+      prebuild: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**'],
+          dest: 'tmp/'
+        }]
+      },
+      postbuild: {
+        files: [{
+          expand: true,
+          cwd: 'tmp',
+          src: ['**'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/events.min.js': ['tmp/events.js'],
+          'dist/deferredevents.min.js': ['tmp/deferredevents.js']
+        }
       }
     }
+
   });
 
-  // grunt.registerTask('test', [
-  //   'connect:test',
-  //   'mocha'
-  // ]);
-
-  grunt.registerTask('server', [
-    'less:dev',
-    'connect:livereload',
-    'watch'
-  ]);
-
-  grunt.registerTask('dev', [
-    'less:dev',
-    'autoprefixer:dev'
-  ]);
-
   grunt.registerTask('dist', [
-    'clean:dist',
-    'less:dist',
-    'autoprefixer:dist'
+    'clean:prebuild',
+    'copy:prebuild',
+    'uglify:dist',
+    'copy:postbuild',
+    'clean:postbuild'
   ]);
 
   grunt.registerTask('default', [
